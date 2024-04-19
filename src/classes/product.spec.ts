ccec6d6e961b2d310bc9617b1464dd127035e2c6
luc4s-git <lucassantos4332@hotmail.com>
2024-04-19 15:31:35 -0300

    testing product.ts

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,13 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number) => new Product(name, price);
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it("Should have property's name and price", () => {
+    const sut = createSut('Jest', 10.5);
+    expect(sut).toHaveProperty('name', 'Jest');
+    expect(sut.price).toBeCloseTo(10.5);
+  });
+});
